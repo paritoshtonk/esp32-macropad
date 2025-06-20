0d7cd207c7f41d5a2d8a7ab67c90fda9ef323b52
paritoshtonk <paritosh.tonk360@gmail.com>
2025-06-20 11:18:53 +0530

    Add default tag

--- main/util.c
+++ main/util.c
@@ -2,5 +2,12 @@
 
 void printDebugLog(char *TAG, char *message)
 {
+    const char *default_tag = "UTIL";
+
+    if (TAG == NULL || strlen(TAG) == 0)
+    {
+        TAG = (char *)default_tag;
+    }
+
     ESP_LOGD(TAG, "%s", message);
 }
