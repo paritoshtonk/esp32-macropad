972c7d1827bd4428d494c84cc504a3b7f7515cd7
paritoshtonk <paritosh.tonk360@gmail.com>
2025-06-20 11:12:29 +0530

    add global file

---
+++ main/global.c
@@ -0,0 +1,4 @@
+#include "global.h"
+
+bool isDeviceConnected = false;
+bool isHIDTaskStarted = false;
--- main/global.h
+++ main/global.h
@@ -1,4 +1,7 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
+extern bool isDeviceConnected;
+extern bool isHIDTaskStarted;
+
 #endif
