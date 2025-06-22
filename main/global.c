ef01893041d623b41b3416c13c61aeabfae1f592
paritoshtonk <paritosh.tonk360@gmail.com>
2025-06-23 01:02:42 +0530

    add queue in global

--- main/global.c
+++ main/global.c
@@ -1,4 +1,10 @@
 #include "global.h"
 
+QueueHandle_t button_queue;
 bool isDeviceConnected = false;
 bool isHIDTaskStarted = false;
+
+void init_queue()
+{
+    button_queue = xQueueCreate(10, sizeof(button_event_t));
+}
--- main/global.h
+++ main/global.h
@@ -2,8 +2,21 @@
 #define GLOBAL_H
 
 #include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+typedef struct
+{
+    char id_char;
+    bool long_press;
+} button_event_t;
+
+extern QueueHandle_t button_queue;
 
 extern bool isDeviceConnected;
 extern bool isHIDTaskStarted;
+
+void init_queue();
 
 #endif
