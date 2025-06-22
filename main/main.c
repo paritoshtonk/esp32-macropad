f96c854999b13cbaba1cd5a285fa34339b6c2c2f
paritoshtonk <paritosh.tonk360@gmail.com>
2025-06-23 01:04:06 +0530

    init queue, dip switch and button

--- main/main.c
+++ main/main.c
@@ -1,7 +1,13 @@
 
 #include "esp_hid_device.c"
+#include "global.h"
+#include "button.c"
+#include "dip.c"
 
 void app_main(void)
 {
+    init_queue();
+    dip_main();
+    button_main();
     esp_hid_device_main();
 }
